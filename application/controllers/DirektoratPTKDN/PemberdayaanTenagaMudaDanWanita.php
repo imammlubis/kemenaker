139ec110be88a3424597d530a6360a95712dd131
imammlubis <imammlubis@gmail.com>
2016-12-21 08:13:59 +0700

    Update Pemberdayaan 1

---
+++ application/controllers/DirektoratPTKDN/PemberdayaanTenagaMudaDanWanita.php
@@ -0,0 +1,83 @@
+<?php
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: imam lubis
+ * Date: 21/12/2016
+ * Time: 8:12
+ */
+class PemberdayaanTenagaMudaDanWanita extends CI_Controller
+{
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->model('lokasipemberdayaantkkmudaModel');
+        $this->load->library('pagination');
+    }
+    function index($offset=0)
+    {
+        $config['total_rows'] = $this->lokasipemberdayaantkkmudaModel->total();
+        $config['base_url'] = base_url()."DirektoratPTKDN/PemberdayaanTenagaMudaDanWanita/Index";
+        $config['per_page'] = 10;
+        $config['uri_segment'] = '4';
+
+        $config['full_tag_open'] = '<div class="dataTables_paginate paging_bootstrap_full_number" id="sample_1_paginate">
+                                                        <ul class="pagination" style="visibility: visible;">';
+        $config['full_tag_close'] = '</ul></div>';
+
+        //$config['first_link'] = '« First';
+        $config['first_tag_open'] = '<li class="prev">';
+        $config['first_tag_close'] = '</li>';
+
+        //$config['last_link'] = 'Last »';
+        $config['last_tag_open'] = '<li class="next">';
+        $config['last_tag_close'] = '</li>';
+
+        //$config['next_link'] = 'Next →';
+        $config['next_tag_open'] = '<li class="next">';
+        $config['next_tag_close'] = '</li>';
+
+        //$config['prev_link'] = '← Previous';
+        $config['prev_tag_open'] = '<li class="prev">';
+        $config['prev_tag_close'] = '</li>';
+
+        $config['cur_tag_open'] = '<li class="active"><a href="">';
+        $config['cur_tag_close'] = '</a></li>';
+
+        $config['num_tag_open'] = '<li class="page">';
+        $config['num_tag_close'] = '</li>';
+        $this->pagination->initialize($config);
+        $query = $this->lokasipemberdayaantkkmudaModel->get(10,$this->uri->segment(4));
+        $data['lokasipemberdayaantkkmudaModel'] = null;
+        if($query){
+            $data['lokasipemberdayaantkkmudaModel'] =  $query;
+        }
+        $data ['main_content'] = 'DirektoratPTKDN/LokasiPemberdayaanTenagaKKMudadanWanita';
+        $this->load->view('layout/MainLayout', $data);
+    }
+
+    function UploadExcel(){
+        $file = $_FILES['upload']['tmp_name'];
+        //load the excel library
+        $this->load->library('excel');
+        //read file from path
+        $objPHPExcel = PHPExcel_IOFactory::load($file);
+        //$col = $objPHPExcel->setActiveSheetIndex(0)->getHighestColumn();
+        $this->db->empty_table('lokasipemberdayaantkkmuda');
+        $rows = $objPHPExcel->setActiveSheetIndex(0)->getHighestRow();
+        $highestColumnIndex = PHPExcel_Cell::columnIndexFromString($objPHPExcel->setActiveSheetIndex(0)->getHighestColumn());
+        for ($i = 2; $i<=$objPHPExcel->setActiveSheetIndex(0)->getHighestRow(); $i++) {
+            $kabupaten = $objPHPExcel->getActiveSheet()->getCellByColumnAndRow(0, $i);
+            $qty = $objPHPExcel->getActiveSheet()->getCellByColumnAndRow(1, $i);
+            $jumlah = $objPHPExcel->getActiveSheet()->getCellByColumnAndRow(2, $i);
+
+            $dataInput = array(
+                'IDKABUPATEN' => $kabupaten,
+                'QTY' => $qty,
+                'JUMLAH' => $jumlah
+            );
+            $this->lokasipemberdayaantkkmudaModel->add($dataInput);
+        }
+        redirect('DirektoratPTKDN/PemberdayaanTenagaMudaDanWanita');
+    }
+}
---
+++ application/views/DirektoratPTKDN/PemberdayaanTenagaMudaDanWanita.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: imam lubis
+ * Date: 21/12/2016
+ * Time: 8:13
+ */
